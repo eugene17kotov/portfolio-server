0e85edcee63c5a8946116acc6a1304ec3337387d
Yevhenii Kobets <eu.kobets@gmail.com>
2023-06-16 13:02:36 +0300

    add wake up route for cron

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const ctrlWrapper = require('../ctrlWrapper.js');
 const contactController = require('../contactController.js');
 
+// wake up route for cron
+router.get('', (req, res) => {
+    return res.status(200).send('I am not sleeping!');
+});
+
 router.post('/contact', ctrlWrapper(contactController));
 
 module.exports = router;
